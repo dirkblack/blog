00715fa3a408afbbe1958c2d44ab275e9ba31aac
Dirk <dirkbritton@gmail.com>
2020-11-04 11:50:24 -0800

    Initial commit

---
+++ src/Providers/ServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace DarkBlog\Providers;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+/**
+ * Service provider
+ */
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadRoutesFrom(__DIR__.'/../../routes.php');
+        $this->loadViewsFrom(__DIR__.'/../views', 'darkblog');
+        $this->publishes([
+            __DIR__.'/../views', resource_path('views/vendor/darkblog')
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
